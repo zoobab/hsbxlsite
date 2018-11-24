9bf424ed0366b20a12ad395f0e7da8e733c2c3ea
betz <behets@gmail.com>
2018-11-24 01:17:03 +0100

    commit message for changelog regular text

--- scripts/commits.sh
+++ scripts/commits.sh
@@ -9,7 +9,7 @@ mkdir -p $2
 find $1 -name "*.md" | while read line; do
     contentline=${line#"$1/"};
     filename=$(echo -n $contentline | md5sum | cut -f 1 -d " ");
-    log=$(git log --pretty=format:'{%n  "commit": "%H",%n  "author": "%aN",%n  "commit_date": "%ci",%n  "message": "%f"%n},' "$line" | perl -pe 'BEGIN{print "["}; END{print "]\n"}' | perl -pe 's/},]/}]/');
+    log=$(git log --pretty=format:'{%n  "commit": "%H",%n  "author": "%aN",%n  "commit_date": "%ci",%n  "message": "%s"%n},' "$line" | perl -pe 'BEGIN{print "["}; END{print "]\n"}' | perl -pe 's/},]/}]/');
      echo $log > "$2/$filename.json";
 done
 
